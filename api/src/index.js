c77d6e29c4849a75ff6ff8a4625c395d3d53b309
JeanRiveraUNI <114692318+JeanRiveraUNI@users.noreply.github.com>
2023-11-25 17:14:26 -0300

    Update dependencies and routes

--- api/src/index.js
+++ api/src/index.js
@@ -1,13 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const v1workoutRouter = require('./v1/routes/workoutRoutes');
-const v1taskRouter = require('./v1/routes/taskRoutes');
+const v2taskRouter = require('./v2/routes/taskRoutes');
+require('dotenv').config();
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//routes
 app.use(express.json())
 app.use("/api/v1/workouts", v1workoutRouter); 
-app.use("/api/v1/tasks", v1taskRouter);
+app.use("/api/v2/tasks", v2taskRouter);
+
+
+//MongoDB connection
+mongoose.connect(
+    process.env.MONGODB_URL)
+    .then(() => {
+        console.log("Connected to MongoDB Atlas successfully!");
+    })
+    .catch((err) => {
+         console.error("Error connecting to MongoDB Atlas:", err);
+    }
+);
+
 
 app.listen(PORT, () => { 
     console.log(`Server is running on port ${PORT}.`); 
